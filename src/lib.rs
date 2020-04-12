53ca4441e91ab07d2f19560a6f9d4be5af04b4e6
David Pollak <feeder.of.the.bears@gmail.com>
2020-04-11 21:29:25 -0700

    A little more playing around

--- src/lib.rs
+++ src/lib.rs
@@ -18,11 +18,13 @@ fn test_foo() {
     let mut hm = foo();
     assert_eq!(hm.get("foo"), Some(&"bar".to_string()));
     assert_eq!(hm.get("moose"), Some(&"goose".to_string()));
+    let hm2 = hm.clone();
     hm = hm.update("foo".to_string(), "sloth".to_string());
     hm = hm.update("dog".to_string(), "woof".to_string());
     assert_eq!(hm.get("foo"), Some(&"sloth".to_string()));
     let baz = foo() + hm ;
     assert_eq!(baz.get("foo"), Some(&"bar".to_string()));
     assert_eq!(baz.get("dog"), Some(&"woof".to_string()));
-
+    assert_eq!(hm2.get("foo"), Some(&"bar".to_string()));
+    assert_eq!(hm2.get("dog"), None);
 }
