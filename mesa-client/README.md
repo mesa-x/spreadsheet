098f3ad15e064e840e9285dcf6f402f7333cf485
David Pollak <feeder.of.the.bears@gmail.com>
2020-07-13 16:37:38 -0700

    Got a basic grid client working

---
+++ mesa-client/README.md
@@ -0,0 +1,9 @@
+# Spreadsheet UI
+
+This is the browser-based UI for Mesa X.
+
+## Libraries
+
+### [React Datasheet](https://github.com/nadbm/react-datasheet)
+
+Slow with 100+ rows
---
+++ mesa-client/src/App.tsx
@@ -0,0 +1,97 @@
+import * as React from 'react';
+import ReactDataSheet from 'react-datasheet';
+import "react-datasheet/lib/react-datasheet.css";
+import { isNumber } from 'util';
+// const { HelloRequest, RepeatHelloRequest,
+//   HelloReply } = require('../../proto/hello_world_pb.js');
+// const { GreeterClient } = require('../../proto/hello_world_grpc_web_pb.js');
+
+export interface GridElement extends ReactDataSheet.Cell<GridElement, number> {
+  value: number | string | null;
+}
+
+class MyReactDataSheet extends ReactDataSheet<GridElement, number> { }
+
+interface AppState {
+  grid: GridElement[][];
+}
+
+//You can also strongly type all the Components or SFCs that you pass into ReactDataSheet.
+let cellRenderer: ReactDataSheet.CellRenderer<GridElement, number> = (props) => {
+  var backgroundStyle: any = {}
+  backgroundStyle.color = isNumber(props.cell.value) && props.cell.value && props.cell.value < 0 ? 'red' : undefined;
+  backgroundStyle.textAlign = isNumber(props.cell.value) ? "right" : 'left'
+  return (
+    <td style={backgroundStyle} onMouseDown={props.onMouseDown} onMouseOver={props.onMouseOver} onDoubleClick={props.onDoubleClick} className="cell">
+      {props.children}
+    </td>
+  )
+}
+
+export default class App extends React.Component<{}, AppState> {
+  inLoop = false
+
+  doLoop() {
+    if (!this.inLoop) {
+      this.inLoop = true
+      setTimeout(() => {
+        this.updateNumberValue(0, 0, x => x + 1)
+
+        this.inLoop = false
+        this.doLoop()
+      }, 1000);
+    }
+  }
+
+  updateNumberValue(row: number, col: number, theFunc: (arg: number) => number) {
+    var grid = this.state.grid
+    if (grid.length > row && grid[row].length > col) {
+      var theVal = grid[row][col].value
+      if (isNumber(theVal)) {
+        theVal = theFunc(theVal)
+        grid[row][col].value = theVal
+        this.setState({ grid })
+        return theVal
+      }
+    }
+
+    return null
+  }
+
+  constructor(props: {}) {
+    super(props)
+    this.state = {
+      grid: [
+        [{ value: 1 }, { value: -3 }],
+        [{ value: -2 }, { value: 4 }]
+      ]
+    }
+
+  }
+  private activateLasers() {
+    this.updateNumberValue(1, 0, x => x + 4)
+  }
+  render() {
+    this.doLoop()
+    return (
+      <div>
+        <button onClick={this.activateLasers.bind(this)}>
+          Activate Lasers
+        </button>
+        <MyReactDataSheet
+          data={this.state.grid}
+          valueRenderer={(cell) => cell.value}
+          onCellsChanged={changes => {
+            const grid = this.state.grid.map(row => [...row])
+            changes.forEach(({ cell, row, col, value }) => {
+              var v2 = Number(value)
+              grid[row][col] = { ...grid[row][col], value: !isNaN(v2) ? v2 : value }
+            })
+            this.setState({ grid })
+          }}
+          cellRenderer={cellRenderer}
+        />
+      </div>
+    )
+  }
+}
